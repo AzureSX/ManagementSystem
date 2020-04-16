2d28849987020f8778738390e1a0e443e9c438d0
azure <974762673@qq.com>
2020-04-16 14:32:28 +0800

    修改name

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ git pull
 npm install && npm run build
 
 #删除容器
-docker rm -f demo1 &> /dev/null
+docker rm -f ManagementSystem &> /dev/null
 
 #启动容器
 docker run -d --restart=on-failure:5\
     -p 8080:80 \
     -v $PWD/dist:/usr/share/nginx/html \
-    --name demo1 nginx
+    --name ManagementSystem nginx
